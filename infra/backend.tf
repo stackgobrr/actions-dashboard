677b3cd3c892616cee4c40e065d0caf669470efb
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2026-01-25 15:54:35 +0000

    feat: configure state locking

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket  = "h3ow3d-tfstate-575108940418"
     # passed in at terraform init
     # key     = "utils/actions-dashboard/${var.environment}/terraform.tfstate"
+    use_lockfile = true
     region  = "eu-west-2"
     encrypt = true
   }
